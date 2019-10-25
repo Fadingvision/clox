a51436873e890a578fc9ebfc43bf57128c84475b
blackLearning <fuyigekua@foxmail.com>
2019-10-25 14:56:59 +0800

    Test vm

--- main.c
+++ main.c
@@ -8,23 +8,25 @@ int main(int argc, const char* argv[]) {
   initVM();                   
   initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 1.2);
-  // Write first instruction: constant                  
-  writeChunk(&chunk, OP_CONSTANT, 222);        
-  // constant的操作数最多只存一个字节，因此最多能保存256(0 - 255)个constant
-  writeChunk(&chunk, constant, 222);
+  // int constant = addConstant(&chunk, 1.2);
+  // // Write first instruction: constant                  
+  // writeChunk(&chunk, OP_CONSTANT, 222);        
+  // // constant的操作数最多只存一个字节，因此最多能保存256(0 - 255)个constant
+  // writeChunk(&chunk, constant, 222);
 
-  // writeConstant
-  writeConstant(&chunk, 3.4, 222);
+  // 1 + 2 * 3 - 4 / -5
+  writeConstant(&chunk, 1, 222);
+  writeConstant(&chunk, 2, 222);
+  writeConstant(&chunk, 3, 222);
+  writeChunk(&chunk, OP_MULTIPLY, 222);
   writeChunk(&chunk, OP_ADD, 222);
-
-  writeConstant(&chunk, 5.6, 222);
-  writeChunk(&chunk, OP_DIVIDE, 222);
-
+  writeConstant(&chunk, 4, 222);
+  writeConstant(&chunk, 5, 222);
   writeChunk(&chunk, OP_NEGATE, 224);
+  writeChunk(&chunk, OP_DIVIDE, 224);
+  writeChunk(&chunk, OP_SUBTRACT, 224);
 
   writeChunk(&chunk, OP_RETURN, 224);
-  // disassembleChunk(&chunk, "test chunk");
 
   // executing instructions
   interpret(&chunk);
